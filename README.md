56e21c51d1cb6d1138fd5704210021cb089bef8d
Daniel Albert <git@esclear.de>
2017-04-07 16:50:22 +0200

    Add license, readme and some type annotations

---
+++ README.md
@@ -0,0 +1,26 @@
+# Simple Raytracer in Racket
+This is a simple ray tracer written in racket for a university programming class.
+The intention was not to write a very efficient, top-of-the-line rendering program,
+but instead work out by myself most of the standard ray-tracing techniques and
+the features of racket.
+
+## What it does
+* Rendering of simple primitives
+  * Spheres
+  * Planes
+  * Triangles
+* Shadows using shadow rays
+* It uses rackets class features, although it is way slower that using plain structs
+
+## Whot is does not
+* There is currently no gamma-correction
+* There is no support for loading in meshes
+* Use multiple threads to speed everything up
+
+## What might be done in the future
+* I might be adding reflection and refraction to the shader
+* Possibly texture support
+  * A skybox might be nice as well
+
+## License
+It is released under the WTFPL (see the LICENSE file in this repository)
--- geometry.rkt
+++ geometry.rkt
@@ -1,31 +1,29 @@
-#lang racket
+#lang typed/racket
 (require racket/struct)
 
 (provide (all-defined-out))
 
-(struct point (x y z)
+;; POINT IN 3D-SPACE
+(struct point ([x : Number] [y : Number] [z : Number])
   #:transparent)
-(struct ray (point direction)
-  #:transparent)
-
-;;
-;; THREE-DIMENSIONAL VECTOR
-;;
-(struct vec (x y z)
-  #:methods gen:custom-write
-  [(define write-proc
-     (make-constructor-style-printer
-      (lambda (obj) 'vec)
-      (lambda (obj) (list (vec-x obj) (vec-y obj) (vec-z obj)))))]
-  #:transparent)
-
+(: origin (-> point))
 (define (origin)
   (point 0 0 0))
 
+;; VECTOR IN 3D-SPACE
+(struct ray ([point : point] [direction : vec])
+  #:transparent)
+
+;; THREE-DIMENSIONAL VECTOR
+(struct vec ([x : Number] [y : Number] [z : Number])
+  #:transparent)
+
+(: zerovec (-> vec))
 (define (zerovec)
   (vec 0 0 0))
 
 ; Get the length of a given vector v
+(: vec-len (-> vec Number))
 (define (vec-len v)
   (let ((a (vec-x v))
         (b (vec-y v))
@@ -33,12 +31,14 @@
     (sqrt (+ (* a a) (* b b) (* c c)))))
 
 ; Add multiple vectors
+(: vec-add (-> vec * vec))
 (define (vec-add . vecs)
   (vec (foldl + 0 (map vec-x vecs))
        (foldl + 0 (map vec-y vecs))
        (foldl + 0 (map vec-z vecs))))
 
 ; Subtract the vectors in vecs from the vector a
+(: vec-sub (-> vec vec * vec))
 (define (vec-sub a . vecs)
   (vec-add a (vec-reverse (apply vec-add vecs))))
 
@@ -48,6 +48,7 @@
 ; cannot be normalized in the sense that the result
 ; has the same "direction" as v, thus (0 0 0) is returned
 ; in this case.
+(: vec-normalize (-> vec vec))
 (define (vec-normalize v)
   (let ((len (vec-len v)))
     (if (zero? len)
@@ -55,22 +56,26 @@
         (vec-scale v (/ len)))))
 
 ; Scale a vector v by the given factor
+(: vec-scale (-> vec Number vec))
 (define (vec-scale v factor)
   (vec (* (vec-x v) factor)
        (* (vec-y v) factor)
        (* (vec-z v) factor)))
 
 ; Reverse a given vector v
+(: vec-reverse (-> vec vec))
 (define (vec-reverse v)
   (vec-scale v (- 1)))
 
 ; Calculate the dot-product of two vectors a and b
+(: vec-dot (-> vec vec Number))
 (define (vec-dot a b)
   (+ (* (vec-x a) (vec-x b))
      (* (vec-y a) (vec-y b))
      (* (vec-z a) (vec-z b))))
 
 ; Calculate the cross-product of two vectors a and b
+(: vec-cross (-> vec vec vec))
 (define (vec-cross a b)
   (let ((xa (vec-x a)) (xb (vec-x b))
                        (ya (vec-y a)) (yb (vec-y b))
@@ -80,12 +85,14 @@
          (- (* xa yb) (* ya xb)))))
 
 ; Convert a point to a vector
+(: point->vec (-> point vec))
 (define (point->vec point)
   (vec (point-x point)
        (point-y point)
        (point-z point)))
 
 ; Convert a vector to a point
+(: vec->point (-> vec point))
 (define (vec->point vec)
   (point (vec-x vec)
          (vec-y vec)
